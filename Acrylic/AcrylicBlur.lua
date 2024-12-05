beaedafd8b09578fdb2be83d6ec8254664e3a1c9
Masterboy123 <76537746+YTMasterboy123YT@users.noreply.github.com>
2024-12-04 20:28:00 -0600

    Update AcrylicBlur.lua

--- Acrylic/AcrylicBlur.lua
+++ Acrylic/AcrylicBlur.lua
@@ -1,11 +1,30 @@
 local AcrylicBlur = {}
+AcrylicBlur.__index = AcrylicBlur
+
+-- Services
 local TweenService = game:GetService("TweenService")
+local RunService = game:GetService("RunService")
+
+-- Constants
+local BLUR_SIZE = 24
+local TWEEN_INFO = TweenInfo.new(0.3, Enum.EasingStyle.Quad)
 
 function AcrylicBlur.new()
-    local blur = Instance.new("BlurEffect")
-    blur.Size = 0
-    blur.Parent = game:GetService("Lighting")
-    return blur
+    local self = setmetatable({}, AcrylicBlur)
+    
+    self.blur = Instance.new("BlurEffect")
+    self.blur.Size = 0
+    self.blur.Parent = game:GetService("Lighting")
+    
+    return self
+end
+
+function AcrylicBlur:Enable()
+    TweenService:Create(self.blur, TWEEN_INFO, {Size = BLUR_SIZE}):Play()
+end
+
+function AcrylicBlur:Disable()
+    TweenService:Create(self.blur, TWEEN_INFO, {Size = 0}):Play()
 end
 
 return AcrylicBlur
